8267bbaa7e071d9931f5f37efdd3d968b8db35b9
Minokinad <danik.ivacewil@bk.ru>
2024-12-25 23:01:52 +0300

    Add wordcount command

--- MyBot/bot.py
+++ MyBot/bot.py
@@ -28,6 +28,10 @@ async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(results)
     await context.bot.answer_inline_query(update.inline_query.id, results)
 
+async def wordcount(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    count = len(context.args)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=count)
+
 if __name__ == '__main__':
     with open("bot_token", mode='r') as file:
         BOT_TOKEN = file.read()
@@ -37,10 +41,12 @@ if __name__ == '__main__':
     echo_handler = MessageHandler(filters=(filters.TEXT & (~filters.COMMAND)), callback=echo)
     caps_handler = CommandHandler('caps', caps)
     inline_caps_handler = InlineQueryHandler(inline_caps)
+    word_count_handler = CommandHandler('wordcount', wordcount)
 
     application.add_handler(echo_handler)
     application.add_handler(start_handler)
     application.add_handler(caps_handler)
     application.add_handler(inline_caps_handler)
+    application.add_handler(word_count_handler)
 
     application.run_polling()
